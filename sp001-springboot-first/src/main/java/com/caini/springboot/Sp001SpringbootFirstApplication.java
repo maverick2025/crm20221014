20fe45022446364f2c283ff5c47ab9a7c2b78fbb
mav <maverick2009@qq.com>
2022-10-14 15:27:22 +0800

    第一次提交，初始化

---
+++ sp001-springboot-first/src/main/java/com/caini/springboot/Sp001SpringbootFirstApplication.java
@@ -0,0 +1,13 @@
+package com.caini.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp001SpringbootFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp001SpringbootFirstApplication.class, args);
+    }
+
+}
---
+++ sp001-springboot-first/src/test/java/com/caini/springboot/Sp001SpringbootFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caini.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp001SpringbootFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
